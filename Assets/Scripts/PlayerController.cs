ace74da7d77fa6e19e5ffd91e7976cb436375d7d
Rafel Bover Lladó <rlladobover@gmail.com>
2021-12-14 10:44:23 +0100

    proyecto_final

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20f;
+    private float turnSpeed = 50f;
+    private float horizontalInput;
+    private float verticalInput;
+    private float limX = 200f;
+    private float yMax = 200f;
+    private float yMin = 0f;
+    private float limZ = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+        transform.Rotate( Vector3.right * Time.deltaTime * turnSpeed * verticalInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (transform.position.x >= limX)
+        {
+            transform.position = new Vector3(limX, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x <= -limX)
+        {
+            transform.position = new Vector3(-limX, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y >= yMax)
+        {
+            transform.position = new Vector3(transform.position.x, yMax, transform.position.z);
+        }
+        if (transform.position.y <= yMin)
+        {
+            transform.position = new Vector3(transform.position.x, yMin, transform.position.z);
+        }
+        if (transform.position.z >= limZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, limZ);
+        }
+        if (transform.position.z <= -limZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -limZ);
+        }
+    }
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    private float rotateSpeed = 500f;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+}
