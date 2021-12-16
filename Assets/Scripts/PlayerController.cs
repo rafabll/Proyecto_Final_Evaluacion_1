d23296c6f0fdf9b9fef310f05c099d81628d53b4
Rafel Bover Lladó <91464437+rafabll@users.noreply.github.com>
2021-12-16 23:48:55 +0100

    PROYECTO FINAL

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = Vector3.zero;
+        transform.position = new Vector3(0, 100, 0);
     }
 
     // Update is called once per frame
